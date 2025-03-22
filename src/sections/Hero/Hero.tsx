545d98e02501e650717de11a3c2c0f1de7ab9118
ElvinYekayev <elvinyeka@gmail.com>
2025-03-22 19:50:27 +0400

    fixed hero image err

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image';
 import './hero.css';
 import Link from 'next/link';
 
 export default function Hero() {
   return (
     <div className="hero">
-      {/* <Image
-        src="/assets/img/slider/main-slider-bg.jpg"
-        alt="Hero Image"
-        width={1200}
-        height={800}
-        className="hero"
-      /> */}
       <h1 className="hero-title">Swift Solutions for Appliance Repair</h1>
       <p className="hero-text">
         Trust Fasteny Appliance Repair for All Your Repair Needs
