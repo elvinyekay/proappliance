d64c1cb9a2ed9ad82d299c63701487c90b44b178
ElvinYekayev <elvinyeka@gmail.com>
2025-04-11 00:34:13 +0400

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://proapplianceexpress.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
